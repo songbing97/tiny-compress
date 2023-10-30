cf875cf175c2300e4df3b2254cae27e8608c6513
宋承亮 <songchengliang@ksztone.com>
2023-10-30 19:20:08 +0800

    fix: 初步实现压缩算法

--- extension.js
+++ extension.js
@@ -1,34 +1,56 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-
+const path = require('path');
+const axios = require('axios');
+const { open } = require('fs/promises');
+const fs = require('fs');
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+	let disposable = vscode.commands.registerCommand('minify_picture', async function (fileUri) {
+		console.log('fileUri:', fileUri)
+		const fileName = path.basename(fileUri.fsPath);
+		vscode.window.showInformationMessage(`开始压缩${fileName}`);
+		const fd = await open(fileUri.fsPath);
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tiny-compress" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tiny-compress.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tiny-compress!');
+		axios.post('https://api.tinify.com/shrink', fs.createReadStream(fileUri.fsPath), {
+			auth: {
+				username: 'api',
+				password: 'g4nmvpYKFptdZGhSZNsg7f593Pn77KP1'
+			},
+			headers: {
+				'Content-Type': 'application/octet-stream',
+			}
+		}).then(res => {
+			if (!res.status.match(/2\d{2}/g)) {
+				vscode.window.showInformationMessage(`压缩${fileUri}失败`);
+			} else {
+				vscode.window.showInformationMessage(`压缩${fileUri}成功`);
+			}
+			console.log('res', res); // res.data.output.url
+		}).catch(err => {
+			console.log('err', err);
+		}).finally(() => {
+			console.log('finally');
+		})
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 function deactivate() {}
+
+function downloadImage(url) {
+	return new Promise((resolve, reject) => {
+		axios.get(url, {
+			responseType: 'stream'
+		}).then(res => {
+			console.log('res:', res)
+		}).catch(err => {
+			console.log('err:', err)
+		})
+	})
+}
 
 module.exports = {
 	activate,
