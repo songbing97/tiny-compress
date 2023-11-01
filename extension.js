12cdce356e3aaff87825411eee8ccf44c10f2483
宋承亮 <songchengliang@ksztone.com>
2023-11-01 17:18:56 +0800

    feat: 初步完成

--- extension.js
+++ extension.js
@@ -1,38 +1,26 @@
 const vscode = require('vscode');
 const path = require('path');
 const axios = require('axios');
-const { open } = require('fs/promises');
 const fs = require('fs');
+// const readChunkPack = require('read-chunk')
+// const {readChunk} = readChunkPack
+const { stat, readdir, unlink } = require('fs/promises');
+// import vscode from 'vscode'
+// import fs from 'fs'
+// import {fileTypeFromBuffer} from 'file-type';
+// import {readChunk} from 'read-chunk';
+// import { stat, readdir, unlink } from 'fs/promises';
+// import path from 'path'
+// import axios from 'axios'
+
 /**
  * @param {vscode.ExtensionContext} context
  */
+
 function activate(context) {
 	let disposable = vscode.commands.registerCommand('minify_picture', async function (fileUri) {
-		console.log('fileUri:', fileUri)
-		const fileName = path.basename(fileUri.fsPath);
-		vscode.window.showInformationMessage(`开始压缩${fileName}`);
-		const fd = await open(fileUri.fsPath);
-
-		axios.post('https://api.tinify.com/shrink', fs.createReadStream(fileUri.fsPath), {
-			auth: {
-				username: 'api',
-				password: 'g4nmvpYKFptdZGhSZNsg7f593Pn77KP1'
-			},
-			headers: {
-				'Content-Type': 'application/octet-stream',
-			}
-		}).then(res => {
-			if (!res.status.match(/2\d{2}/g)) {
-				vscode.window.showInformationMessage(`压缩${fileUri}失败`);
-			} else {
-				vscode.window.showInformationMessage(`压缩${fileUri}成功`);
-			}
-			console.log('res', res); // res.data.output.url
-		}).catch(err => {
-			console.log('err', err);
-		}).finally(() => {
-			console.log('finally');
-		})
+    console.log(fileUri.fsPath)
+		startMinify(fileUri.fsPath)
 	});
 
 	context.subscriptions.push(disposable);
@@ -40,16 +28,89 @@ function activate(context) {
 
 function deactivate() {}
 
-function downloadImage(url) {
-	return new Promise((resolve, reject) => {
-		axios.get(url, {
-			responseType: 'stream'
-		}).then(res => {
-			console.log('res:', res)
-		}).catch(err => {
-			console.log('err:', err)
-		})
-	})
+async function startMinify(path) {
+  const isDir = await isDirectory(path)
+  if (isDir) {
+    recurDirectory(path, minifyFile)
+  } else {
+    minifyFile(path)
+  }
+}
+
+async function minifyFile(path) {
+  try {
+    const res = await axios.post('https://api.tinify.com/shrink', fs.createReadStream(path), {
+      auth: {
+        username: 'api',
+        password: 'g4nmvpYKFptdZGhSZNsg7f593Pn77KP1'
+      },
+      headers: {
+        'Content-Type': 'application/octet-stream',
+      }
+    })
+    console.log('res.data.output.url', res.data.output.url)
+    await replaceOldFile(res.data.output.url, path)
+    return true
+  } catch (err) {
+    console.error(err)
+		vscode.window.showErrorMessage(`文件${path}压缩失败。`);
+		return false
+  }
+}
+
+async function replaceOldFile(url, path) {
+  const response = await axios.get(url, { responseType: 'stream' })
+  console.log(response.status)
+  if (response.status === 200) {
+    console.log('path', path)
+    const result = await unlink(path)
+    const fileStream = fs.createWriteStream(path)
+    response.data.pipe(fileStream)
+    response.data.on('end', () => {
+      fileStream.end();
+      vscode.window.showInformationMessage(`文件${path}已压缩成功。`);
+    });
+  }
+}
+
+async function recurDirectory(paths, fn) {
+  try {
+    const files = await readdir(paths)
+    console.log('files:', files, paths)
+    for (let i = 0; i < files.length; i++) {
+      const currentFile = path.resolve(paths, files[i])
+      console.log('currentFile: ', currentFile)
+      const isDir = await isDirectory(currentFile)
+      if (isDir) {
+        await recurDirectory(currentFile, fn)
+      } else {
+        const isFile = await isValidFile(currentFile)
+        if (isFile) {
+          await fn(currentFile)
+        }
+      }
+      
+    }
+    return true
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function isDirectory (path) {
+  const stats = await stat(path)
+  return stats.isDirectory()
+}
+
+// async function isFile (path) {
+//   const stats = await stat(path)
+//   return stats.isFile()
+// }
+
+async function isValidFile(path) {
+  const ext = path.split('.').pop()
+  const validExts = ['png', 'jpeg']
+  return validExts.includes(ext)
 }
 
 module.exports = {
